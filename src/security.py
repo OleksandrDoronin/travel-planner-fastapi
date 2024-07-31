8468c65a12124ff6947f9e1ba7a2385272b3921e
Oleksandr Doronin <oleksandr.doron@gmail.com>
2024-07-31 07:40:13 +0300

    setting up tokens

--- src/security.py
+++ src/security.py
@@ -1,5 +1,10 @@
-from passlib.context import CryptContext
+from datetime import datetime, timezone
+from datetime import timedelta
+from typing import Optional
 
+import jwt
+from passlib.context import CryptContext
+from config import settings
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -12,3 +17,19 @@ def get_password_hash(password: str) -> str:
 def verify_password(password, hashed_password) -> bool:
     """Password verification"""
     return bcrypt_context.verify(password, hashed_password)
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+    to_encode.update({"exp": expire})
+    to_encode["id"] = data.get("id")
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
+    return encoded_jwt
