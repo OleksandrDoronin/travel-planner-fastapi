c0f47c9cdf88e4afe46002890f8829bb8ca8ba9b
OleksandrDoronin <trqazx@gmail.com>
2024-11-02 10:19:24 +0200

    create model user and social account

--- src/models/users.py
+++ src/models/users.py
@@ -1,21 +1,38 @@
-import uuid
+from datetime import datetime
+from typing import Optional
+
+from sqlalchemy import DateTime, ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database import Base
-from sqlalchemy import Boolean, Column, String
-from sqlalchemy.dialects.postgresql import UUID
 
 
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    username = Column(String, unique=True, index=True, nullable=False)
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    is_active = Column(Boolean, default=True)
-    phone_number = Column(String, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    full_name: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True, index=True)
+    profile_picture: Mapped[Optional[str]]
+    bio: Mapped[Optional[str]]
+    gender: Mapped[Optional[str]]
+    is_active: Mapped[bool] = mapped_column(default=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
 
-    def __repr__(self):
-        return f'<User(id={self.id}, username={self.username}, email={self.email})>'
+    social_account = relationship('SocialAccount', back_populates='user', uselist=False)
+
+
+class SocialAccount(Base):
+    __tablename__ = 'social_accounts'
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    service: Mapped[str]
+    social_account_id: Mapped[str] = mapped_column(unique=True)
+    access_token: Mapped[Optional[str]] = mapped_column(unique=True)
+    refresh_token: Mapped[Optional[str]] = mapped_column(unique=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id', ondelete='CASCADE'))
+
+    user = relationship('User', back_populates='social_account')
