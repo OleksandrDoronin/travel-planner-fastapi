623754831d800161f900de6ef5cf2b457cd457bd
OleksandrDoronin <trqazx@gmail.com>
2024-11-09 10:49:43 +0200

    add and settings middleware

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
-from auth.routers import auth, user
 from fastapi import FastAPI
 
+from src.auth.routers import auth, user
+from src.middleware import setup_middleware
 
-app = FastAPI()
 
-app.include_router(user.router)
-app.include_router(auth.router)
+app = FastAPI(title='Travel planner API')
+setup_middleware(app)
+
+
+app.include_router(user.router, prefix='/api/v1')
+app.include_router(auth.router, prefix='/api/v1')
---
+++ src/middleware.py
@@ -0,0 +1,20 @@
+from config import get_settings
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+from starlette.middleware.sessions import SessionMiddleware
+
+
+settings = get_settings()
+
+
+def setup_middleware(app):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*'],
+    )
+
+    app.add_middleware(GZipMiddleware, minimum_size=1000)
+    app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
